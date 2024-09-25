d8442c314257c53f030a0b243c17b5ab31304d1a
Kristian Thaulow <kristian1990@gmail.com>
2024-09-25 20:05:30 +0200

    Provide markdown

--- FireQuestClient/src/app/app.config.ts
+++ FireQuestClient/src/app/app.config.ts
@@ -6,11 +6,13 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { provideMarkdown } from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(), 
+    provideMarkdown(),
     provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync(), provideCharts(withDefaultRegisterables())
   ]
 };
